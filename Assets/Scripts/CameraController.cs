42fb81ccf7ee24bf1227a8e1820d3840a09cab43
jonschwa <jschwartz@bluestatedigital.com>
2018-01-28 14:13:56 -0500

    new areana scene, physics starting to work

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject stone; 
+	private Vector3 offset; 
+
+	void Start () 
+	{
+		offset = transform.position - stone.transform.position;
+	}
+
+	// LateUpdate is called after Update each frame
+	void LateUpdate () 
+	{
+		// Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+		transform.position = stone.transform.position + offset;
+	}
+}
--- Assets/Scripts/Stone.cs
+++ Assets/Scripts/Stone.cs
@@ -4,28 +4,64 @@ using UnityEngine;
 
 public class Stone : MonoBehaviour {
 
-	public float speed = 15.0F;
+	public float moveSpeed;
 	public Vector3 moveDirection;
+	public float moveTime; 
 	public float gravity = 20.0F;
 
 	private Rigidbody rb;
-	private SphereCollider collider;
+	private BoxCollider collider;
 	private CharacterController character;
+	private bool isMoving = false;
+	private bool endOfMove = false;
+	private bool startMoving = false;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
-		collider = GetComponent<SphereCollider>();
+		collider = GetComponent<BoxCollider>();
 		character = GetComponent<CharacterController>();
+	}
+
+	void CalculateSpeedAndDirection () {
+		//static for now
+		//moveSpeed = 200.0f;
+
+		//setting this to straight up right now for simplicity
+		moveDirection = new Vector3(0, 0, 1);
+	}
+		
+	//to update the visual indicator of the power selected
+	void DisplayPower(int amount) {
+
+	}
+
+	void Shoot (Vector3 direction, int speed) {
+
+	}
+
+	void CheckIfEndOfMove() {
+
+	}
+
+	void FinishTurn() {
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			moveDirection = transform.TransformDirection(moveDirection);
-			moveDirection *= speed;
+		if (!isMoving) {
+			CalculateSpeedAndDirection ();
+			//moveDirection = transform.TransformDirection (moveDirection);
+			startMoving = true;
+		}
+	}
 
-		//moveDirection.y -= gravity * Time.deltaTime;
-		character.Move(moveDirection * Time.deltaTime);
+	void FixedUpdate() {
+		if (startMoving && !isMoving) {
+			Debug.Log ("starting to move!");
+			rb.AddForce (moveDirection * moveSpeed, ForceMode.Impulse);
+			isMoving = true;
+		}
 	}
 }
