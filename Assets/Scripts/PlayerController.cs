cce437d03660804ab539a7de84cb1a537c0cb0d0
jonschwa <jschwartz@bluestatedigital.com>
2018-01-31 22:38:09 -0500

    add playercontroller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public int currentRoundScore = 0;
+	public int currentTurnScore = 0;
+	public Text scoreText;
+	public int playerNumber;
+
+
+	void Awake() {
+		
+	}
+
+	public void CalculateRoundScore()
+	{
+		//round score can be calculated by adding the scores of all the child stones
+		UpdateScoreText(currentRoundScore);
+	}
+
+	public void UpdateTurnScore(int scoreChange) {
+		currentTurnScore += scoreChange;
+		UpdateScoreText(currentTurnScore);
+	}
+
+	void UpdateScoreText(int score) {
+		scoreText.text = "Player " + playerNumber + " Score: " + score;	
+	}
+		
+
+}
